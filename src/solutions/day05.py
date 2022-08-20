97f23696f8599c73b650c2fa90b51f12eb7787af
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-20 22:18:37 +1000

    Solve Day 5 Part 2

--- src/solutions/day05.py
+++ src/solutions/day05.py
@@ -2,6 +2,7 @@
 Solutions for AOC 2018 Day 5 - "Alchemical Reduction".
 """
 
+import re
 import string
 
 
@@ -14,7 +15,7 @@ def process_input_file(filepath="./input/day05.txt"):
         return file.read().strip()
 
 
-def solve_part1(polymer):
+def solve_part1(polymer: str):
     """
     Solves AOC 2018 Day 5 Part 1 // Returns the length of the polymer resulting
     from reacting the given polymer.
@@ -23,27 +24,36 @@ def solve_part1(polymer):
     return len(new_polymer)
 
 
-def solve_part2(_polymer):
+def solve_part2(polymer: str):
     """
-    Solves AOC 2018 Day 5 Part 2 // ###
+    Solves AOC 2018 Day 5 Part 2 // Returns the length of the shortest polymer
+    resulting from fully removing all of one type of unit and reacting the
+    improved polymer.
     """
-    return NotImplemented
+    min_length = None
+    for char in string.ascii_lowercase:
+        # Improve polymer by removing all of a single type of unit
+        improved_polymer = re.sub(fr"({char}|{char.upper()})", "", polymer)
+        # Fully react the improved polymer and check if it is the shortest so far
+        reacted_polymer = react_polymer(improved_polymer)
+        if min_length is None or len(reacted_polymer) < min_length:
+            min_length = len(reacted_polymer)
+    return min_length
 
 
-def react_polymer(polymer):
+def react_polymer(polymer: str):
     """
     Reacts the given polymer, returning the resulting polymer.
     """
-    pairs = []
-    for char in string.ascii_lowercase:
-        pairs.append(f"{char}{char.upper()}")
-        pairs.append(f"{char.upper()}{char}")
     new_polymer = str(polymer)
     while True:
         no_reaction = True
-        for pair in pairs:
+        for char in string.ascii_lowercase:
             old_len = len(new_polymer)
-            new_polymer = new_polymer.replace(pair, "")
+            # Remove unit pairs of same type and different polarity
+            new_polymer = new_polymer.replace(f"{char}{char.upper()}", "")
+            new_polymer = new_polymer.replace(f"{char.upper()}{char}", "")
+            # Check if reaction occured
             if old_len > len(new_polymer):
                 no_reaction = False
         if no_reaction:
