a826cb210a5eb6b536df13827c7f3f5cf0771339
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-08-23 21:44:24 +1000

    Solve Day 6 Part 1

--- test/test_solutions.py
+++ test/test_solutions.py
@@ -4,7 +4,7 @@ against the actual problem input files. This test module is configured to use
 the pytest library.
 """
 
-from src.solutions import day01, day02, day03, day04, day05
+from src.solutions import day01, day02, day03, day04, day05, day06
 
 
 def test_day01_part1():
@@ -95,3 +95,21 @@ def test_day05_part2():
     input_data = day05.process_input_file()
     solution = day05.solve_part2(input_data)
     assert solution == 6874
+
+
+def test_day06_part1():
+    """
+    Solution test method for AOC 2018 Day 6 Part 1.
+    """
+    input_data = day06.process_input_file()
+    solution = day06.solve_part1(input_data)
+    assert solution == 5941
+
+
+def test_day06_part2():
+    """
+    Solution test method for AOC 2018 Day 6 Part 2.
+    """
+    input_data = day06.process_input_file()
+    solution = day06.solve_part2(input_data)
+    assert solution == 40244
