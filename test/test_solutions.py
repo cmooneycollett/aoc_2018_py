b69b6da31227d83c85e16adce9f80700e6a22623
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-09-04 16:13:16 +1000

    Solve Day 9 Part 1

--- test/test_solutions.py
+++ test/test_solutions.py
@@ -5,7 +5,7 @@ the pytest library.
 """
 
 from src.solutions import day01, day02, day03, day04, day05, day06, day07, \
-    day08
+    day08, day09
 
 
 def test_day01_part1():
@@ -150,3 +150,21 @@ def test_day08_part2():
     input_data = day08.process_input_file()
     solution = day08.solve_part2(input_data)
     assert solution == 25007
+
+
+def test_day09_part1():
+    """
+    Solution test method for AOC 2018 Day 9 Part 1.
+    """
+    input_data = day09.process_input_file()
+    solution = day09.solve_part1(input_data)
+    assert solution == 412127
+
+
+def test_day09_part2():
+    """
+    Solution test method for AOC 2018 Day 9 Part 2.
+    """
+    input_data = day09.process_input_file()
+    solution = day09.solve_part2(input_data)
+    assert solution == 3482394794
