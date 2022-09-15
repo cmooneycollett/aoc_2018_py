b5774d15f2b989ee75b871a890664fc6aea5773d
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-09-15 20:59:27 +1000

    Solve Day 12 Part 1

--- src/main.py
+++ src/main.py
@@ -147,6 +147,19 @@ def solve_day11():
     print("============================================================")
 
 
+def solve_day12():
+    """
+    Solves AOC 2018 Day 12 Parts 1 and 2, printing out the results.
+    """
+    print("AOC 2018 Day 12 - \"Subterranean Sustainability\"")
+    input_data = day12.process_input_file()
+    p1_solution = day12.solve_part1(input_data)
+    print(f"> P1 solution - {p1_solution}")
+    p2_solution = day12.solve_part2(input_data)
+    print(f"> P2 solution - {p2_solution}")
+    print("============================================================")
+
+
 if __name__ == "__main__":
     # Import to allow execution from project top-level directory
     import os
@@ -154,17 +167,18 @@ if __name__ == "__main__":
     sys.path.append(os.getcwd())
     # Solution module imports
     from src.solutions import day01, day02, day03, day04, day05, day06, day07, \
-        day08, day09, day10, day11
+        day08, day09, day10, day11, day12
     # Main solver methods
     print("============================================================")
-    solve_day01()
-    solve_day02()
-    solve_day03()
-    solve_day04()
-    solve_day05()
-    solve_day06()
-    solve_day07()
-    solve_day08()
-    solve_day09()
-    solve_day10()
-    solve_day11()
+    # solve_day01()
+    # solve_day02()
+    # solve_day03()
+    # solve_day04()
+    # solve_day05()
+    # solve_day06()
+    # solve_day07()
+    # solve_day08()
+    # solve_day09()
+    # solve_day10()
+    # solve_day11()
+    solve_day12()
