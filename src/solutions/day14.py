57580a538be5e9214641d1b52c250ed4ebf6de46
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-09-24 16:52:32 +1000

    Solve Day 14 Part 2

--- src/solutions/day14.py
+++ src/solutions/day14.py
@@ -30,8 +30,25 @@ def solve_part1(num_recipes):
     return recipes[num_recipes:num_recipes + 10]
 
 
-def solve_part2(_num_recipes):
+def solve_part2(num_recipes):
     """
-    Solves AOC 2018 Day 14 Part 2 // ###
+    Solves AOC 2018 Day 14 Part 2 // Determines the number of recipes that
+    appear to the left of the given score sequence.
     """
-    return NotImplemented
+    score_sequence = str(num_recipes)
+    elf1_i = 0
+    elf2_i = 1
+    recipes = "37"
+    while True:
+        left = len(recipes) - len(score_sequence)
+        if left >= 0:
+            if recipes[left - 1:len(recipes) - 1] == score_sequence:
+                return left - 1
+            if recipes[left:len(recipes)] == score_sequence:
+                return left
+        elf1_recipe = int(recipes[elf1_i])
+        elf2_recipe = int(recipes[elf2_i])
+        new_recipes = str(elf1_recipe + elf2_recipe)
+        recipes += new_recipes
+        elf1_i = (elf1_i + 1 + elf1_recipe) % len(recipes)
+        elf2_i = (elf2_i + 1 + elf2_recipe) % len(recipes)
